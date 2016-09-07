158716e832eae79dfc93f7034adc82e16d67dd1c
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-09-07 13:07:40 -0400

    displayed user's name

---
+++ src/assignment01/Problem1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fostp4040
+ */
+public class Problem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your Name");
+        String name = input.nextLine();
+        System.out.println("Hello "+ name);
+    }
+}
