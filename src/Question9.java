76d2b77f16bcb355e79fc81733514391f7ed5019
Hadida <Hadida@192.168.2.21>
2016-09-07 21:21:52 -0400

    Serenity for Guanity

---
+++ src/Question9.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hadida
+ */
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] words = new String[10];
+        
+        System.out.println("Please enter 10 words");
+        for (int i = 0; i < 10; i++) {
+            String word = input.nextLine();
+            words[i] = word;
+            
+            
+            int count = word.length();
+            
+            
+        }
+        
+        for (int i = 1; i < 10; i++) {
+            String first = words[0];
+            
+        }
+        
+        
+        
+    }
+}
