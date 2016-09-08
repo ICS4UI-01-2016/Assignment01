21582f106f6fcd2e90ec962973632b620665a8d1
hadik9595 <hadik9595@HRH-IC0027296.wrdsb.ca>
2016-09-08 11:19:41 -0400

    Kevin Yu is a league God

---
+++ src/Question10.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadik9595
+ */
+public class Question10 {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/Question9.java
+++ src/Question9.java
@@ -15,23 +15,22 @@ public class Question9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] words = new String[10];
-        
+        int total = 0;
+ 
         System.out.println("Please enter 10 words");
         for (int i = 0; i < 10; i++) {
             String word = input.nextLine();
-            words[i] = word;
-            
-            
-            int count = word.length();
-            
-            
+            words[i] = word;    
         }
         
-        for (int i = 1; i < 10; i++) {
-            String first = words[0];
-            
-        }
         
+        for (int i = 0; i < 10; i++) {
+            String first = words[i];
+            int count = first.length();
+            total = count + total;   
+        }
+        //prints ou
+        System.out.println(total);
         
         
     }
