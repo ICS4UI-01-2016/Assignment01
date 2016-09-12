a6a1c9d325039bc88ac85124bb82eff34042c15c
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2016-09-12 13:25:27 -0400

    l

---
+++ src/A1Q7.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sevcm7279
+ */
+public class A1Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // tell user initial population and growth rate
+        System.out.println("The initial population in 1999 was 7 billion");
+        System.out.println("With a growth rate of 1.4% per year...");
+        System.out.println("The population will reach 10 billion");
+        
+        // calculate how many years until pop is 10 bil
+        double pop = 
+        
+    }
+}
