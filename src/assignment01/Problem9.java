4beac6a2af1bc6a5ea4c069fb2a259230f28a7a2
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-11-22 14:08:50 -0500

    working on problem 9

---
+++ src/assignment01/Problem9.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment01;
+import java.util.Scanner;
+
+/**
+ *
+ * @author fostp4040
+ */
+public class Problem9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String word=input.nextLine();
+        for(int i = 0; i < 9; i++)
+        {
+            String[] wordList = null;
+            System.out.println(word.length());
+            wordList[i]=word;
+            if(i==9){
+                
+            }
+        }
+        
+    }
+}
